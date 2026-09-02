a79c44ca1df9facfd9c444ae14f54a23f640e025
JacobB14210 <128636188+JacobB14210@users.noreply.github.com>
2026-09-01 19:37:33 -0700

    Created a calendar interface

--- frontend/src/components/calendar/calendar.jsx
+++ frontend/src/components/calendar/calendar.jsx
@@ -1,10 +1,35 @@
 import { Dashboard } from "../component-index";
+import{ Calendar as AriaCalendar, CalendarGrid, CalendarCell, Heading, Button } from "react-aria-components";
+import { useDateFormatter } from "react-aria/useDateFormatter";
+import { today, getLocalTimeZone } from "@internationalized/date";
+import { useState } from "react";
+
+import "./calendar.css"
 
 export function Calendar() {
+    let [date, setDate] = useState(today(getLocalTimeZone()));
+
     return (
         <div>
             <Dashboard />
-            Calendar Page
+            <h1>Calendar Page</h1>
+
+            <AriaCalendar
+                aria-label="Time Off Calendar"
+                value={date}
+                onChange={setDate}
+            >
+                <header>
+                    <Button slot="previous">Previous</Button>
+                    <Button slot="next">Next</Button>
+                </header>
+                <Heading />
+                <CalendarGrid>
+                    {(date) => (
+                        <CalendarCell date={date} />
+                    )}
+                </CalendarGrid>
+            </AriaCalendar>
         </div>
     );
 }
