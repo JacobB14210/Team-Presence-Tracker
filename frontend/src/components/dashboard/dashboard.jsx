00580bbed98e685d2dc06aea642d203e02323587
JacobB14210 <128636188+JacobB14210@users.noreply.github.com>
2026-09-16 14:50:46 -0700

    Changed dashboard and added logo

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,24 +1,30 @@
 import { useNavigate } from "react-router-dom";
 
+import "./dashboard.css"
+import logo from "../../assets/PBUSDLogo.png"
+
 export function Dashboard() {
 
     const navigate = useNavigate();
 
     return (
-        <div>
-            <button
-                type="button"
-                onClick={() => navigate("/Time-Off")}
-            >
-                Time Off Request
-            </button>
+        <div className="dashboard-container">
+            <img src={logo} alt="BPUSD-logo" />
+            
+            <div className="dashboard-buttons">
+                <button
+                    type="button"
+                    onClick={() => navigate("/Time-Off")}>
+                    Time Off Request
+                </button>
 
-            <button
-                type="button"
-                onClick={() => navigate("/Calendar")}
-            >
-                Calendar
-            </button>
+                <button
+                    type="button"
+                    onClick={() => navigate("/Calendar")}>
+                    Calendar
+                </button>
+            </div>
+            
         </div>
     );
 }
