0199974c741cfd03ed50db020d68484cce595b61
JacobB14210 <128636188+JacobB14210@users.noreply.github.com>
2026-09-24 16:25:15 -0700

    Started the approve page

--- frontend/src/components/approve/approve-page.jsx
+++ frontend/src/components/approve/approve-page.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import { Dashboard } from "../component-index";
+
+import "./approve-page.css"
 
 export function Approve() {
 
+
     return (
         <div className="approve-container">
-            Approve Page
+            <Dashboard />
+            <h1>Time Off Approve</h1>
+            <div className="requests">
+                <h2>Pending Time Off Requests</h2>
+                Populate With List of Requests
+            </div>
         </div>
     );
 }
